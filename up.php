087845fa32014adf89f5dc2e2e8ea6b71f6aab93
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2020-11-17 13:52:00 +0300

    Delete up.php

--- up.php
+++
@@ -1,6 +0,0 @@
-<?php
-$smsham = file_get_contents("https://raw.githubusercontent.com/termux-lab/smsham/master/smsham.py");
-$file = fopen("smsham.py", "w");
-fwrite($file, $smsham);
-fclose($file); 
-?>
